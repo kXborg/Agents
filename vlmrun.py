ac1ccb6c7051119345a28849c3a9921dcf9d3ea2
kXborg <kukilp213@gmail.com>
2025-09-24 19:28:21 +0530

    successful run

--- vlmrun.py
+++ vlmrun.py
@@ -4,6 +4,10 @@ import torch
 from PIL import ImageGrab
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+# Setup fail-safes
+pyautogui.PAUSE = 2.5
+pyautogui.FAILSAFE = True
+
 
 def load_model(model_name):
     moondream = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True, dtype=torch.bfloat16, device_map={"": "cuda"})
@@ -12,44 +16,58 @@ def load_model(model_name):
     return moondream
 
 
-def main():
-    model = load_model("moondream/moondream3-preview")
-
+def grab_click(query, model):
     # Capture current screen
     screenshot = ImageGrab.grab()  # PIL image
+    height = screenshot.height
+    width = screenshot.width
     screenshot.save("screen.png")
 
     # Ask Moondream to locate WhatsApp icon
-    query = "WhatsApp Icon"
     response = model.point(screenshot, query)
 
     print("Model response:", response)
 
-    {'points': [{'x': 0.294921875, 'y': 0.9794921875}]}
-
-    
-
-    abs_x = int(point['x'] * width)
-    abs_y = int(point['y'] * height)
-
     coords = None
     try:
-        coords = (int(response["x"]), int(response["y"]))
+        point1 = response["points"][0]
+        abs_x = int(point1['x'] * width)
+        abs_y = int(point1['y'] * height)
+        coords = [abs_x, abs_y]
     except Exception:
         print("Could not parse response. Got:", response)
 
-    # # --- 4. Use PyAutoGUI to click ---
-    # if coords:
-    #     print(f"Clicking at {coords}")
-    #     pyautogui.moveTo(coords[0], coords[1], duration=0.5)
-    #     pyautogui.click()
-    # else:
-    #     print("No valid coordinates, skipping click.")
+    # Use PyAutoGUI to click
+    if coords:
+        print(f"Clicking at {coords}")
+        pyautogui.moveTo(coords[0], coords[1], duration=0.5)
+        pyautogui.click()
+    else:
+        print("No valid coordinates, skipping click.")
 
-    # # --- 5. (Optional) Type message ---
-    # time.sleep(2)  # wait for chat to open
-    # pyautogui.typewrite("Happy Birthday!", interval=0.05)
-    # pyautogui.press("enter")
+    time.sleep(2)
+
+
+def main():
+    model = load_model("moondream/moondream3-preview")
+
+    grab_click("WhatsApp Icon", model)
+    grab_click("Sovit DeepVidya", model)
+
+    screenshot = ImageGrab.grab()
+
+    capt = model.query(screenshot, "What do you think about Sovit's profile pic?")
+    ans = capt["answer"]
+    print(ans)
+
+    greet_text = "Hi this is my first attempt on Agents!"
+
+    pyautogui.typewrite(greet_text, interval=0.05)
+    pyautogui.press("enter")
+
+    pyautogui.typewrite(ans, interval=0.05)
+    pyautogui.press("enter")
+
 
 if __name__ == '__main__':
     main()
