82db5d7d9680eaa4fba2502d17cb1c20ddf9d1f8
bhomik749 <bhomiksharma749@gmail.com>
2025-10-09 20:03:30 +0530

    TODO: Directory Parsing

--- scripts/github_repo_parser.py
+++ scripts/github_repo_parser.py
@@ -1,80 +1,141 @@
 import os
+import re
 import json
 import requests
 from typing import List, Dict
 from dotenv import load_dotenv
 import xml.etree.ElementTree as ET
+from typing import List, Optional, Pattern, Union
 
 load_dotenv()
 
 #providing access to github token
 TOKEN = os.getenv('GITHUB_TOKEN')
-headers = {"Authorization": f"token {TOKEN}"}
-
 #separating common part of github repo
-api_git_url = "https://api.github.com/repos/"
+ROOT_DIR = "https://api.github.com/repos/"
 
-#instantiating context variable to store names and meta data of imp. directories along with file_names to ignore
-context = list(dict())
-ignore_files = [".gitignore", ".github"]
+class GitRepoParser():
+    def __init__(
+            self,
+            github_token: bool,
+            max_file_size: int = 100*1024, #bytes
+    ):
+        """
+        Args:
+          github_token: For private repos
+          max_file_size: skip files larger than this
+          include_patterns: list of filename to include
+          exclude_patterns: list of filename to exclude
+        """
+        #using session object to define default values and persist as well
+        self.s = requests.Session()
+        if github_token:
+            self.s.headers.update({"Authorization": f"token {TOKEN}"})
+        self.max_file_size = max_file_size
+        self.include_ext = [".py", ".md", ".toml", ".yaml", ".json", ".txt"]
+        self.exclude_ext = [".gitignore", ".git", ".pdf", ".vscode", ".docker", ".docstr", ".docstr.yaml", ".github"]
 
-#main function to parse the url
-def parser(url: str):
-    """
-    This utility is used to 
-    parse github repo, such that
-    it doesn't exceeds input context
-    length. Also llm can find it easier
-    to read and understand the contents
-    from the JSON format.
-    """
-
-    if isinstance(url, str):
-        url_parts = url.split("https://github.com/")[-1].split('/')
-        name = "/".join(url_parts[:2])
-        contents_url = f"{api_git_url}{name}/contents"
-        # print(f"Repo user/project: {url_parts}")
-        # print(f"Repo Name: {name}")
-        try:
-            response = requests.get(contents_url, headers=headers, timeout = 30)
-            response.raise_for_status()
-            # print(f"response text: {type(response_txt)}")
-            try: 
-                files = response.json()
-                # print(f"Extracted JSON: \n{files}")
-                return files, name
+    
+    def url_parse(self, repo_url: str):
+        if isinstance(repo_url, str):
+            url_parts = repo_url.split("https://github.com/")[-1].split('/')
+            name = "/".join(url_parts[:2])
+            contents_url = f"{ROOT_DIR}{name}/contents/"
+            # print(contents_url)
+            # print(contents_url)
+            # print(f"Repo user/project: {url_parts}")
+            # print(f"Repo Name: {name}")
+            try:
+                response = self.s.get(contents_url)
+                response.raise_for_status()
+                self.contents = response.json()
+                # print(f"response text: {type(response_txt)}") 
+                return self.contents
+                # Except Exception as e:
+                    # print(f"error in generating json content: {e}")
+                    # return name
+            except requests.exceptions.HTTPError as e:
+                print(f"Unable to access the URL: {e}\n")
+            except requests.exceptions.RequestException as e:
+                print(f"Unable to fetch the directory: {e}\n")
             except Exception as e:
-                print(f"error in generating json content: {e}")
-            return name
-        except requests.exceptions.HTTPError as e:
-            print(f"Unable to access the URL: {e}\n")
-        except requests.exceptions.RequestException as e:
-            print(f"Unable to fetch the directory: {e}\n")
-        except Exception as e:
-            print(f"Unknown error: {e}\n")
-    else:
-        raise TypeError("Kindly provide a string as input.")
-    
-def key_dir_dict(content):
-    try:
-        for file in content:
-            if file["name"] in ignore_files:
-                continue
-            else:
-                context.append({"name_dir": file['name'], "type_dir": file['type'], "html_url": file["html_url"] })
-        return context
-        # print(f"context list: {context}\n")
-        # print(f"context length: {len(context)}\n")
-    except TypeError as e:
-        print(content, "object is not iterable.")
+                print(f"Unknown error: {e}\n")
+        else:
+            raise TypeError("Kindly provide a string as input.")
+        
+    def recursive_dir(self, dir_url, sha):
+        if isinstance(dir_url, str):
+            url_parts = dir_url.split("https://api.github.com/")[-1].split('/')
+            name = "/".join(url_parts[1:3])
+            print(dir_url)
+            print(name)
+            contents_url = f"{ROOT_DIR}{name}/git/trees/{sha}"
+            response = self.s.get(dir_url)
+            self.contents = response.json()
+            print(f"{self.contents}\n\n\n")
+            # print(contents_url)
+            # print(contents_url)
+            # print(f"Repo user/project: {url_parts}")
+            # print(f"Repo Name: {name}")
+            try:
+                response = self.s.get(contents_url)
+                response.raise_for_status()
+                self.contents = response.json()
+                # print(f"{self.contents}\n\n\n") 
+                return self.contents
+                # Except Exception as e:
+                    # print(f"error in generating json content: {e}")
+                    # return name
+            except requests.exceptions.HTTPError as e:
+                print(f"Unable to access the URL: {e}\n")
+            except requests.exceptions.RequestException as e:
+                print(f"Unable to fetch the directory: {e}\n")
+            except Exception as e:
+                print(f"Unknown error: {e}\n")
+        else:
+            raise TypeError("Kindly provide a string as input.")
+        
+    def fetch_content(self, parsed_list):
+        # print(parsed_list)
+        self.context = []
+        if not parsed_list:
+            raise print("Url provided is not parsed. Use url_parse() attribute")
+        for content in parsed_list:
+            # print(content["type"] if content["type"] == "dir" else None)
+            print("Content Type in content list: ", content["type"])
+            if content["type"] == 'dir': #and not any(content["name"].endswith(ext) for ext in self.exclude_ext):
+                if any(content["name"].endswith(ext) for ext in self.exclude_ext):
+                    continue
+                else:
+                    print("Included file types: ", content["name"])
+                    print("type of download_url: ", type(content["html_url"]))
+                    parse_content= self.recursive_dir(content["html_url"], content["sha"])
+                    self.fetch_content(parse_content)
+                    
+                    # print(f"\n\nPrint of Funct fetch_content:\n{self.context}\n\n")
+            elif any(content["name"].endswith(ext) for ext in self.include_ext):
+                # print("included file types: ", content["name"])
+                file_content = self.s.get(content["download_url"]).text
+                self.context.append({
+                    "name_dir": content["name"],
+                    "path": content["path"],
+                    "item": file_content[:5000]
+                })
+        print(self.context)
+        with open("context_txt.txt", 'w') as f:
+            for item_dict in self.context:
+                # f.write(f"{item_dict}\n\n\n")
+                f.write(f"{item_dict["name_dir"]}: {re.sub(r'[\n\t\r]', ' ', item_dict['item'])}\n\n\n")
+        
+        return self.context
 
-content, repo_name = parser("https://github.com/facebookresearch/dinov2")
-item_dict = key_dir_dict(content)
-# print(item_dict)
 
-for dict_ in iter(item_dict):
-    if dict_['name_dir'] == 'README.md':
-        # print(dict_['html_url'])
-        readme_text = parser(dict_['html_url'])
-        print(readme_text)
-    
+grp = GitRepoParser(github_token = True)
+items = grp.url_parse("https://github.com/rasbt/reasoning-from-scratch")
+items_ = grp.fetch_content(items)
+# print(items_)
+
+# with open("context_txt.txt", 'r') as f:
+#     file_ = f.read()
+#     print(file_)
+
