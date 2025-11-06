bd1ed98e88de9fa86580bf2a737263e4153107b3
bhomik749 <bhomiksharma749@gmail.com>
2025-11-06 18:23:08 +0530

    Updated for metadata tree sturcutre

--- scripts/github_repo_parser.py
+++ scripts/github_repo_parser.py
@@ -1,6 +1,9 @@
+"""
+Script to parse Repository and store contents
+in textual form.
+"""
 import os
-import re
-import base64
+import json
 import requests
 from dotenv import load_dotenv
 
@@ -9,91 +12,88 @@ load_dotenv()
 TOKEN = os.getenv('GITHUB_TOKEN')
 ROOT_DIR = "https://api.github.com/repos/"
 
-class GitRepoParser():
+class GitRepoParser:
     def __init__(
             self,
-            github_token: bool,
+            github_token: bool = False,
     ):
         #using session object to define default values and persist as well
         self.s = requests.Session()
         if github_token:
             self.s.headers.update({"Authorization": f"token {TOKEN}"})
-        self.include_ext = [".py", ".md", ".toml", ".yaml", ".json", ".txt"]
-        self.exclude_ext = [".gitignore", ".git", ".pdf", ".vscode", ".docker", ".docstr", ".docstr.yaml", ".github"]
-        self.context = []
+        self.exclude_ext = [".gif", ".jpg",".jpeg", ".png", ".mp4", ".gitignore", ".git",
+                            ".pdf", ".vscode", ".docker", ".docstr", ".docstr.yaml", ".github"]
         
+    def _is_excluded(self, name: str):
+        return any(name.endswith(ext) for ext in self.exclude_ext)
     
-    def url_parse(self, repo_url: str):
+    def _get_extension(self, name: str):
+        return os.path.splitext(name)[1] if "." in name else ""
+    
+    def _get_repo_name(self, repo_url: str):
+        """
+        Converts github repo URL into directory name.
+        """
         if isinstance(repo_url, str):
             url_parts = repo_url.split("https://github.com/")[-1].split('/')
             name = "/".join(url_parts[:2])
-            contents_url = f"{ROOT_DIR}{name}/contents/"
             try:
-                response = self.s.get(contents_url)
-                response.raise_for_status()
-                return response.json()
+                # print(f"Name of the repository directory: {name}")
+                return name
             except requests.exceptions.HTTPError as e:
                 print(f"Unable to access the URL: {e}\n")
-            except requests.exceptions.RequestException as e:
-                print(f"Unable to fetch the directory: {e}\n")
         else:
             raise TypeError("Kindly provide a string as input.")
         
-    def recursive_dir(self, dir_url, sha):
-        if isinstance(dir_url, str):
-            url_parts = dir_url.split("https://api.github.com/")[-1].split('/')
-            name = "/".join(url_parts[1:3])
-            contents_url = f"{ROOT_DIR}{name}/git/trees/{sha}?recursive=1"
-            try:
-                response = self.s.get(contents_url)
-                response.raise_for_status()
-                return response.json()
-            except requests.exceptions.HTTPError as e:
-                print(f"Unable to access the URL: {e}\n")
-            except requests.exceptions.RequestException as e:
-                print(f"Unable to fetch the directory: {e}\n")
-            except Exception as e:
-                print(f"Unknown error: {e}\n")
-        else:
-            raise TypeError("Kindly provide a string as input.")
-    
-    def tree_parser(self, parse_content):
-    
-        for dir in parse_content["tree"]:
-            if dir['type'] == "blob":
-                res = self.s.get(dir['url']).json()
-                base64_string = res['content']
-                base64_bytes = base64_string.encode("ascii")
-                sample_string_bytes = base64.b64decode(base64_bytes)
-                values = sample_string_bytes.decode("utf-8")
-                self.context.append({
-                                    "name_dir": dir["path"],
-                                    "item": {re.sub(r'[\\\n\t\r]', ' ', values)}\
-                                    })
-        return self.context
+    def get_dir_tree(self, repo_url, branch: str = "main"):
+        """
+        Returns nested tree structure of repository
+        with each leaf node representing a particular 
+        file type along with meta data.
+        """
+        repo_name = self._get_repo_name(repo_url)
+        contents_url = f"{ROOT_DIR}{repo_name}/git/trees/{branch}?recursive=1"
+        # print(f"Contents URL variable contains: {contents_url}")
+        print(f"Fetching directory tree for {repo_name}")
+
+        response = self.s.get(contents_url)
+        if response.status_code != 200:
+            raise ValueError(f"Error: {response.status_code} - {response.text}")
         
-    def fetch_content(self, parsed_list):
-        if not parsed_list:
-            raise ValueError("Url provided is not parsed. Use url_parse() attribute")
-        for content in parsed_list:
-            if isinstance(content, str):
+        tree_content = response.json().get("tree", [])
+        # print(f"Tree contents variable contains: {tree_content}")
+        metadata = {}
+        for content in tree_content:
+            if content["type"] != "blob":
                 continue
-            if content["type"] == 'dir'and not any(content["name"].endswith(ext) for ext in self.exclude_ext):
-                    parse_content= self.recursive_dir(content["git_url"], content["sha"])
-                    self.tree_parser(parse_content)
-            elif content["type"]=='file' and any(content["name"].endswith(ext) for ext in self.include_ext):
-                file_content = self.s.get(content["download_url"]).text
-                self.context.append({
-                    "name_dir": content["name"],
-                    "item": file_content
-                })
+            path = content["path"]
+            ext = self._get_extension(path)
 
-        with open("llm_prompt.txt", 'w') as f:
-            for item in self.context:
-                f.write(f"{item["name_dir"]}: {item['item']}\n\n\n")
+            if self._is_excluded(path):
+                continue
 
-        return self.context
+            meta = {
+                "path": path,
+                "type": "file",
+                "ext": ext,
+                "size_kb": round(content.get("size", 0)/ 1024, 2),
+                "url": content["url"]
+            }
+            parts = path.split("/")
+            # print(f"Contents of Parts variable: {parts}")
+            cursor = metadata
+            for folder in parts[:-1]:
+                folder_key = folder + "/"
+                if folder_key not in cursor:
+                    # print(f"folder key variable contains: {folder_key}")
+                    cursor[folder_key] = {}
+                cursor = cursor[folder_key]
+                cursor[parts[-1]] = meta
+        print(f"Repository metadata tree created {len(tree_content)} total items.")
+        return metadata
 
-grp = GitRepoParser(github_token = True)
-items = grp.url_parse("https://github.com/simonw/files-to-prompt")
-items_ = grp.fetch_content(items)
+
+if __name__ == "__main__":
+    parser = GitRepoParser(github_token=True)
+    tree = parser.get_dir_tree("https://github.com/bhomik749/vlm-bench")
+    print(json.dumps(tree, indent = 2))
